3b5f62977a2606da4dcc52a1ad0e50aeb29c6a7a
puffer <pufferdev0807@gmail.com>
2022-08-15 09:37:18 +0900

    complete lesson1

--- FrontEnd/src/screens/authentication/LoginPage.jsx
+++ FrontEnd/src/screens/authentication/LoginPage.jsx
@@ -1,29 +1,48 @@
-import React, { useState, useEffect } from "react";
+import React, { useState,} from "react";
+import Loading from "../../components/loading";
 
 export default function LoginPage(props) {
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
 
     const handleUsernameChange = (e) => {
-
+        setUsername(e.target.value);
     };
 
     const handlePassword = (e) => {
-
+        setPassword(e.target.value);
     };
+
+    const userLogin = (e) => {
+        e.preventDefault();
+        if(username === "")
+            return alert("Please fill the user name")
+        if(password === "")
+            return alert("Please fill the password");
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+        }, 3000)
+        
+    }
 
     return (
         <div>
+            { loading === true ? <Loading /> : null }
             <div className="login-panel">
                 <form>
                     <div className="form-group">
                         <label>Username</label>
-                        <input onChange={handleUsernameChange} />
+                        <input type="text" onChange={handleUsernameChange} value={username} />
                     </div>
                     <div className="form-group">
                         <label>Password</label>
-                        <input type="password" />
+                        <input type="password" onChange={handlePassword} value={password} />
+                    </div>
+                    <div className="form-group">
+                        <button onClick={userLogin}>Login</button>  
                     </div>
                 </form>
             </div>
